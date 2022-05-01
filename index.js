fb893d22f7f76f6d7f843a18fe41ca6946d7272e
Chiu Yein <chiuyein@yahoo.com>
2022-05-01 23:41:19 +0800

    import orbit, create orbit controls, and elapsedTime for rotation animation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { OrbitControls } from "OrbitControls";
+
 //scene
 const scene = new THREE.Scene();
 
@@ -21,9 +23,7 @@ camera.position.z = 3;
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
 //material
-const material = new THREE.MeshBasicMaterial({
-  color: "red",
-});
+const material = new THREE.MeshNormalMaterial();
 
 //mesh
 const box = new THREE.Mesh(boxGeometry, material);
@@ -36,3 +36,28 @@ renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 renderer.render(scene, camera);
+
+//controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const clock = new THREE.Clock();
+
+//animate
+function animate(){
+    const elapsedTime = clock.getElapsedTime();
+    //rotation
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+
+    box.position.x = Math.cos(elapsedTime) * 3;
+    box.position.y = Math.sin(elapsedTime) * 3;
+    
+    //update
+    controls.update();
+
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
